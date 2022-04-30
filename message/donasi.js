e9dd19066004b8a8366ec93b020a32fe065fc594
Kenzst <kenzstore21@gmail.com>
2022-05-01 01:04:01 +0700

    selfbot2

---
+++ message/donasi.js
@@ -0,0 +1,28 @@
+exports.dodo = (prefix) => {
+return`
+*╭─▸*「 *DONASI* 」
+*┴*
+*│* Bantu Developer ngembangin bot ini
+*│* dengan donasi seiklasnya 🙂
+*│*
+*│* Shopee : 081216014295
+*│* Gopay : 081216014295
+*│* Dana : 081216014295
+*│* Ovo : 081216014295
+*│*
+*│* Makasih yang udah donasi
+*│* dan bantu ngembangin bot ini
+*│* moga rejekinya lancar amin
+*┬*
+*╰────────────[*`
+}
+
+
+
+
+
+
+
+
+
+
---
+++ message/info1.js
@@ -0,0 +1,25 @@
+exports.bot = (calender, wa_version, mcc, mnc, os_version, device_manufacturer, device_model,os,ram2,privat,groups,totalchat,latensi,gender,fake,nomerOwner)=> {
+return`
+「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭 」
+
+*▸* Nama : ${fake}
+*▸* Gender : ${gender}
+*▸* Private Chat : ${privat.length}
+*▸* Group Chat : ${groups.length}
+*▸* Total Chat : ${totalchat.length}
+*▸* Speed : ${latensi.toFixed(4)} second
+*▸* Penggunaan Ram : ${ram2}
+*▸* Hostname : ${os.hostname()}
+*▸* Platform : ${os.platform()}
+*▸* Device Model: ${device_model}
+*▸* Device Manufactur : ${device_manufacturer}
+*▸* Wa Version: ${wa_version}
+*▸* Os Version: ${os_version}
+*▸* Owner : wa.me/${nomerOwner}
+*▸* Tgl sekarang : ${calender}
+*▸* Image : Girl Front Line G11
+*▸* Voice : Nina kawai`
+}
+
+
+
